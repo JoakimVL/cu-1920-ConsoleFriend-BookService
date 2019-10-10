e08672e5d71745c267a2fe192ad8f04759b918e2
Joakim VL <joakim.van.laethem@student.howest.be>
2019-10-10 13:43:18 +0200

    forked project from HBO5

---
+++ BookServiceMvc/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BookServiceMvc.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public T GetApiResult<T>(string uri)
+        {
+            using (HttpClient httpClient = new HttpClient()) 
+            { 
+                Task<String> response = httpClient.GetStringAsync(uri); 
+                return Task.Factory.StartNew(() => JsonConvert.DeserializeObject<T>(response.Result)).Result; 
+            } 
+        }
+    }
+}
